1008c6f5684d90da1e64a003b8d86fe2dbb3c6af
CoolCoder119 <143909488+CoolCoder119@users.noreply.github.com>
2024-09-06 15:29:38 +0000

    Pending changes exported from your codespace

---
+++ main.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+
+canvas.width = width;
+canvas.height = height;
+
+/*
+function fillRect(x,y,width,height,color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,width,height);
+}
+
+for (var i=1;i<10;i++) {
+    fillRect((i*width)/10,(i*height)/10,10,10,"white");
+}*/
+
+
+
+
+
